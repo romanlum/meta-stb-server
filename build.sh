763dcf2f3d44ea5ad4697224d26937b12dbf5932
Roman Lumetsberger <romanlu@gmail.com>
2020-10-18 20:00:58 +0200

    update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
+
 #!/bin/sh
+sudo apt-get install -y autoconf automake bison bzip2 cvs diffstat flex g++ gawk gcc gettext git-core gzip help2man ncurses-bin ncurses-dev libc6-dev libtool make texinfo patch perl pkg-config subversion tar texi2html wget zlib1g-dev chrpath libxml2-utils xsltproc libglib2.0-dev python-setuptools zip info coreutils diffstat chrpath libproc-processtable-perl libperl4-corelibs-perl sshpass default-jre default-jre-headless java-common libserf-dev qemu quilt
+git clone git://github.com/oe-alliance/build-enviroment.git -b 4.4
+
+ln -s $(pwd)/recipes-stb-server $(pwd/build-enviroment/meta-local/recipes-local/
+ln -s $(pwd)/conf/distro $(pwd/build-enviroment/meta-local/conf/
+
 export DISTRO=stbserver
 export MACHINE=gbx34k
 make image
+
