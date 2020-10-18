3a759fc83b93ddab34d2fa4c4e2ab6794eb66b28
Roman Lumetsberger <roman.lumetsberger@outlook.com>
2020-10-18 21:26:41 +0200

    udpate build script

--- build.sh
+++ build.sh
@@ -3,10 +3,16 @@
 sudo apt-get install -y autoconf automake bison bzip2 cvs diffstat flex g++ gawk gcc gettext git-core gzip help2man ncurses-bin ncurses-dev libc6-dev libtool make texinfo patch perl pkg-config subversion tar texi2html wget zlib1g-dev chrpath libxml2-utils xsltproc libglib2.0-dev python-setuptools zip info coreutils diffstat chrpath libproc-processtable-perl libperl4-corelibs-perl sshpass default-jre default-jre-headless java-common libserf-dev qemu quilt
 git clone git://github.com/oe-alliance/build-enviroment.git -b 4.4
 
-ln -s $(pwd)/recipes-stb-server $(pwd)/build-enviroment/meta-local/recipes-local/
-ln -s $(pwd)/conf/distro $(pwd)/build-enviroment/meta-local/conf/
+mkdir -p $(pwd)/build-enviroment/meta-stbserver/conf
+
+cp -R $(pwd)/recipes-stb-server $(pwd)/build-enviroment/meta-stbserver/recipes-local/
+cp -R $(pwd)/conf/* $(pwd)/build-enviroment/meta-stbserver/conf/
+
+
 
 cd build-enviroment
+sed -i -e 's/meta-local/meta-stbserver/g' Makefile
+
 export DISTRO=stbserver
 export MACHINE=gbx34k
 make image
