f9afe2d9afa689591a653c295ffacdab1d5857c1
Roman Lumetsberger <romanlum@users.noreply.github.com>
2020-11-23 15:47:27 +0100

    added apt update

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 
 #!/bin/sh
+sudo apt-get update
 sudo apt-get install -y autoconf automake bison bzip2 cvs diffstat flex g++ gawk gcc gettext git-core gzip help2man ncurses-bin ncurses-dev libc6-dev libtool make texinfo patch perl pkg-config subversion tar texi2html wget zlib1g-dev chrpath libxml2-utils xsltproc libglib2.0-dev python-setuptools zip info coreutils diffstat chrpath libproc-processtable-perl libperl4-corelibs-perl sshpass default-jre default-jre-headless java-common libserf-dev qemu quilt
 git clone git://github.com/oe-alliance/build-enviroment.git -b 4.4
 
