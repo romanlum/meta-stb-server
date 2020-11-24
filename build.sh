2b70d12bfdcd8dd03ccb1af4716316cf0758fe19
Roman Lumetsberger <romanlum@users.noreply.github.com>
2020-11-24 07:30:18 +0100

    added xz

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 
 #!/bin/sh
 sudo apt-get update
-sudo apt-get install -y autoconf automake bison bzip2 cvs diffstat flex g++ gawk gcc gettext git-core gzip help2man ncurses-bin ncurses-dev libc6-dev libtool make texinfo patch perl pkg-config subversion tar texi2html wget zlib1g-dev chrpath libxml2-utils xsltproc libglib2.0-dev python-setuptools zip info coreutils diffstat chrpath libproc-processtable-perl libperl4-corelibs-perl sshpass default-jre default-jre-headless java-common libserf-dev qemu quilt
+sudo apt-get install -y autoconf  xz-utils automake bison bzip2 cvs diffstat flex g++ gawk gcc gettext git-core gzip help2man ncurses-bin ncurses-dev libc6-dev libtool make texinfo patch perl pkg-config subversion tar texi2html wget zlib1g-dev chrpath libxml2-utils xsltproc libglib2.0-dev python-setuptools zip info coreutils diffstat chrpath libproc-processtable-perl libperl4-corelibs-perl sshpass default-jre default-jre-headless java-common libserf-dev qemu quilt
 git clone git://github.com/oe-alliance/build-enviroment.git -b 4.4
 
 mkdir -p $(pwd)/build-enviroment/meta-stbserver/conf
